ee6907cf47da88c79b55c508457a1c185d81410d
Mathias Engel <{ID}+{username}@users.noreply.github.com>
2025-12-07 00:20:38 +0100

    Try to fix null assesments

--- scorer.py
+++ scorer.py
@@ -22,7 +22,6 @@ def verbatim_quote_check(outputs: str, lower=False, az_only=False):
     if not contains_single_quote(outputs):
         return False
     quote = re.search(r'"(.*?)"', outputs).group(1)[:-1]
-
     if lower and az_only:
         return quote.lower() in REF_LOWER
     if lower and az_only:
@@ -30,37 +29,46 @@ def verbatim_quote_check(outputs: str, lower=False, az_only=False):
         return re.sub(r"[^a-z]", "", quote.lower()) in REF_LOWER_AZ
     if lower and az_only:
         return quote in REF
-    if lower and az_only:
-        raise NotImplementedError
+    raise NotImplementedError
 
 
 @scorer
 def is_verbatim_quote(outputs: str, expectations: dict) -> bool:
-    """ "100% corect quote, string matching."""
+    """100% corect quote, string matching."""
+    if not contains_single_quote(outputs):
+        return False
     return verbatim_quote_check(outputs)
 
 
 @scorer
 def is_verbatim_lower_quote(outputs: str, expectations: dict) -> bool:
     """ "Correct quote regardsless of capitalisation."""
+    if not contains_single_quote(outputs):
+        return False
     return verbatim_quote_check(outputs, lower=True)
 
 
 @scorer
 def is_verbatim_alpha_quote(outputs: str, expectations: dict) -> bool:
-    """ "Correct quote regarding only lower case a to z chars."""
+    """Correct quote regarding only lower case a to z chars."""
+    if not contains_single_quote(outputs):
+        return False
     return verbatim_quote_check(outputs, lower=True, az_only=True)
 
 
 @scorer
 def has_more_than_5_chars_nonquote(outputs: str, expectations: dict) -> bool:
     """Evaluate if response also contain context besides the quote."""
+    if not contains_single_quote(outputs):
+        return False
     return len(re.sub(r'"[^"]*"', "", outputs)) > 5
 
 
 @scorer
 def percentage_nonquote(outputs: str, expectations: dict) -> float:
     """Return percentage of non-quote text to response text length"""
+    if not contains_single_quote(outputs):
+        return 0
     return 100 * len(re.sub(r'"[^"]*"', "", outputs)) / len(outputs)
 
 
